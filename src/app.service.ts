73478e306bfc57e76cc9bdf76519cd9ea546b3f9
jaleisme <jaleisme.id@gmail.com>
2023-12-14 11:13:28 +0700

    Adding API documentation for auth

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `<code>Please check out the full API documentation at <span style="font-weight: bolder; color: red; background-color: #fceceb; padding: 4px 6px;">/api</span></code>`;
   }
 }
