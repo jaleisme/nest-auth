44699f034a3273da351e289a769eb6d73294adbb
jaleisme <jaleisme.id@gmail.com>
2023-12-15 13:55:02 +0700

    Added API Docs for task management

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `<code>Please check out the full API documentation at <span style="font-weight: bolder; color: red; background-color: #fceceb; padding: 4px 6px;">/api</span></code>`;
+    return `<code>Please check out the full API documentation at <a href="/api" style="font-weight: bolder; color: red; background-color: #fceceb; padding: 4px 6px;">/api</a></code>`;
   }
 }
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -2,35 +2,174 @@ import { Body, Controller, Delete, Get, HttpStatus, NotFoundException, Param, Po
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/task.dto';
 import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { ApiBody, ApiHeader, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller('task')
 export class TaskController {
     constructor(private taskService: TaskService){}
 
     @Post('create')
+    @UseGuards(JwtGuard)
+    @ApiOperation({
+        'summary': 'Create a new task data',
+        'description': 'Create and save a new task data into the database',
+        // 'requestBody': 
+    })
+    @ApiHeader({
+        'name': 'Authorization',
+        'required': true,
+        'description': 'Bearer token for authorization'
+    })
+    @ApiBody({
+        "schema": {
+            'type': 'object',
+            'properties': {
+                'title': {'type': 'string'},
+                'description': {'type': 'string'},
+                'due_date': {'type': 'date', example: '2023-12-13T06:48:49.444Z'},
+            }
+        } 
+    })
+    @ApiOkResponse({
+        status: 200,
+        description: 'Successfully create and save the task data',
+        schema:{
+            example:{
+                "id": 7,
+                "title": "Test Dev Local",
+                "description": "Testing local development environment",
+                "due_date": "2023-12-13T06:48:49.444Z"
+            }
+        }
+    })
     async createTask(@Body() dto: CreateTaskDto){
         return await this.taskService.create(dto);
     }
 
+
+
     @Get()
+    @UseGuards(JwtGuard)
+    @ApiOperation({
+        'summary': 'Retrieve all task data',
+        'description': 'Retrieve and return all task data from the database',
+        // 'requestBody': 
+    })
+    @ApiHeader({
+        'name': 'Authorization',
+        'required': true,
+        'description': 'Bearer token for authorization'
+    })
+    @ApiOkResponse({
+        status: 200,
+        description: 'Successfully retrieve all task data',
+        schema:{
+            example:{
+                "tasks":
+                [
+                    {
+                        "id": 1,
+                        "title": "Test Dev Local Update",
+                        "description": "Testing local development environment",
+                        "due_date": "2023-12-13T06:48:49.444Z"
+                    },
+                    {
+                        "id": 2,
+                        "title": "Test 1",
+                        "description": "Test desc",
+                        "due_date": "2023-12-13T06:48:49.444Z"
+                    }
+                ]
+            }
+        }
+    })    
     async getAllTask(){
         const tasks = await this.taskService.getAllTask();
         return tasks;
     }
 
+
+
     @Get(':id')
+    @UseGuards(JwtGuard)
+    @ApiOperation({
+        'summary': 'Get a specific task data',
+        'description': 'Get a specific task data from the database',
+        // 'requestBody': 
+    })
+    @ApiHeader({
+        
+            'name': 'Authorization',
+            'required': true,
+            'description': 'Bearer token for authorization'
+        
+    })
+    @ApiOkResponse({
+        status: 200,
+        description: 'Successfully fetch and return the task data from the database',
+        schema:{
+            example:{
+                "id": 7,
+                "title": "Test Dev Local",
+                "description": "Testing local development environment",
+                "due_date": "2023-12-13T06:48:49.444Z"
+            }
+        }
+    })
     async getTaskById(@Param() params){
         const id: number = +params.id;
         return await this.taskService.getTaskById(id);
     }
 
     @Put(':id')
+    @UseGuards(JwtGuard)
+    @ApiOperation({
+        'summary': 'Edit a specific task data',
+        'description': 'Edit and update a specific task data from the database',
+        // 'requestBody': 
+    })
+    @ApiHeader({
+        
+            'name': 'Authorization',
+            'required': true,
+            'description': 'Bearer token for authorization'
+        
+    })
+    @ApiOkResponse({
+        status: 200,
+        description: 'Successfully edit and update the task data in the database',
+        schema:{
+            example:{
+                "id": 7,
+                "title": "Test Dev Local",
+                "description": "Testing local development environment",
+                "due_date": "2023-12-13T06:48:49.444Z"
+            }
+        }
+    })
     async updateTask(@Param() params, @Body() req){      
         const id: number = +params.id;  
         return this.taskService.updateTask(req, id);
     }
 
     @Delete(':id')
+    @UseGuards(JwtGuard)
+    @ApiOperation({
+        'summary': 'Delete a task data',
+        'description': 'Delete a specific task data from the database',
+        // 'requestBody': 
+    })
+    @ApiHeader({
+        
+            'name': 'Authorization',
+            'required': true,
+            'description': 'Bearer token for authorization'
+        
+    })
+    @ApiOkResponse({
+        status: 200,
+        description: 'Successfully delete the task data in the database',
+    })    
     async deleteTask(@Param() params){
         const id: number = +params.id;
         const task = this.taskService.getTaskById(id);
